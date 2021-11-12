0a28b67c143ad2c2d51cb394692a54bf4f0d7b4e
Dima Sanin <34384550+dimastar2310@users.noreply.github.com>
2021-11-12 13:21:36 +0200

    Update Calls_List.py

--- Calls_List.py
+++ Calls_List.py
@@ -9,6 +9,14 @@ class Calls_List:
 
 
 
+    def load_csv(self, csv_file):
 
+     rows = []
+     with open(csv_file,'r') as file:
 
-
+        csvreader =csv.reader(file)
+        for row in csvreader:
+            o=CallForElevator(name=row[0], time=row[1], src=row[2], dst=row[3], x=0, allc=0)
+            rows.append(o)
+     self.rows_list =rows
+ 
