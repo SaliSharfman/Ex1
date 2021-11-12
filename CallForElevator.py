6a432b391709ba3634607246001fa0c29110ca52
Dima Sanin <34384550+dimastar2310@users.noreply.github.com>
2021-11-12 17:51:32 +0200

    Update CallForElevator.py

--- CallForElevator.py
+++ CallForElevator.py
@@ -1,4 +1,5 @@
-import csv
+
+
 class CallForElevator:
 
     def __init__(self, name='', time=0, src=0, dst=0, x=0, allc=0) -> None:
@@ -13,15 +14,4 @@ class CallForElevator:
     def __repr__(self)->str:
         return f":{self.name}\n time:{self.time} src:{self.src} "
 
-    def load_csv(self, csv_file):
-     calls = []
-     rows = []
-     with open(csv_file,'r') as file:
 
-        csvreader =csv.reader(file)
-        for row in csvreader:
-            o=CallForElevator(name=row[0], time=row[1], src=row[2], dst=row[3], x=0, allc=0)
-            calls.append(o)
-            rows.append(row)
-    #print(calls[1].time)
-    #print(calls)
