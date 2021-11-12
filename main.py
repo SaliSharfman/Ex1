3d82ec8ad4122d69dc4cc72a7cf70a1231653ede
Dima Sanin <34384550+dimastar2310@users.noreply.github.com>
2021-11-12 13:22:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-
-
 from Building import Building
 from Elevators import Elevators
 import json
@@ -10,18 +8,12 @@ from  Calls_List import Calls_List
 def main():
     #b = Building()
     b = Building("B2.json")
-    print(b.minFloor)
+    print(b.elevators)
 
-    call = CallForElevator()
-    call.load_csv("Calls_a.csv")
-    print(call)
+    #call = CallForElevator()
+   # call.load_csv("Calls_a.csv")
+   # print(call)
 
+    call_l = Calls_List("Calls_a.csv")
+    print(call_l.rows_list)
 
-
-
-
-
-
-
-if __name__ == '__main__':
-    main()
