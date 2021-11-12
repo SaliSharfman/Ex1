bcd6705cbf3df996f4766f2de743c7ef6559a53f
SaliSharfman <salishar2@gmail.com>
2021-11-12 22:50:45 +0200

    call for elevator change 1

--- CallForElevator.py
+++ CallForElevator.py
@@ -1,7 +1,15 @@
+from typing import Final
+
+INIT: Final = 0
+GOING2SRC: Final = 1
+GOIND2DEST: Final = 2
+DONE: Final = 3
+UP: Final = 1
+DOWN: Final = -1
 
 class CallForElevator:
 
-    def __init__(self, name='', time=0, src=0, dst=0, x=0, allc=0) -> None:
+    def _init_(self, name='', time=0, src=0, dst=0, x=0, allc=-1) -> None:
         self.name=name
         self.time=time
         self.src =src
@@ -9,8 +17,21 @@ class CallForElevator:
         self.x=x
         self.allc=allc
 
+    def getState(self) -> int:
+        pass
+    def getTime(self) -> float:
+        pass
+    def getSrc(self) -> int:
+        return self.src
+    def getDest(self) -> int:
+        return self.dst
+    def getType(self) -> int:
+        if (self.src>self.dst):
+            return DOWN
+        else:
+            return UP
+    def allocatedTo(self) -> int:
+        pass
 
-    def __repr__(self)->str:
-        return f":{self.name}\n time:{self.time} src:{self.src} "
-
-
+    def _repr_(self)->str:
+        return f"{self.name} {self.time} {self.src} {self.dst} {self.x} {self.allc}\n "
