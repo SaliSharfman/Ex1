56b1cc68e269ea7eff19ddbe829fdd1ebc2b1fc7
Dima Sanin <34384550+dimastar2310@users.noreply.github.com>
2021-11-16 01:51:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,47 @@
 
 from Building import Building
 from  Calls_List import Calls_List
-from Our_Algo import Our_Algo
+from Ex1 import Ex1
 from CallForElevator import  CallForElevator
+import sys
 def main():
     #b = Building()
-    b = Building("B2.json")
-    print(b.elevators)
-    print("elevator size in building is " +str(b.ElevSize()))
 
-    calls = Calls_List("Calls_a.csv")
+    #print(b.elevators)
+    #print("elevator size in building is " +str(b.ElevSize()))
 
-    print("the size for calls list is = "+str(calls.cals_size()))
-    print(calls.rows_list)
 
-    calls.set_to_scv()
+
+
+
   #  one_call = CallForElevator(0,0,0,0)
 
    # call_l = Calls_List("Calls_a.csv")
     #print(call_l.rows_list)
     #OA = Our_Algo(b,call_l)
+    #Ex1.py
+    #B1.json
+    #C2.csv
+    #out.csv
+    firstargument = sys.argv[0]
+    secondargument = sys.argv[1]
+    thirdargument = sys.argv[3]
+
+    fourdargument = sys.argv[4]
+
+    calls = Calls_List(secondargument)
+    b = Building(thirdargument)
+    #we will work on this function
+    #in Ex1
+    #calls.set_to_scv(fourdargument)
+    #Ex1.py B1.json C2.csv out.csv
+    syses = []
+    syses.append(firstargument)
+    syses.append(secondargument)
+    syses.append(thirdargument)
+    syses.append(fourdargument)
+    print(syses)
+      
+    #our_algo = Ex1(calls,b,fourdargument)
 
 
-
-
-
-
-
-
-if __name__ == '__main__':
-    main()
