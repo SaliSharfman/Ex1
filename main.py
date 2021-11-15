6ee50570cdc212e8dfdaf6b79f7f424361002660
Dima Sanin <34384550+dimastar2310@users.noreply.github.com>
2021-11-15 20:57:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,20 @@
 from Building import Building
 from  Calls_List import Calls_List
 from Our_Algo import Our_Algo
-
+from CallForElevator import  CallForElevator
 def main():
     #b = Building()
     b = Building("B2.json")
     print(b.elevators)
-    print("elevator size is " +str(b.ElevSize()))
+    print("elevator size in building is " +str(b.ElevSize()))
 
-    #call = CallForElevator()
-    # call.load_csv("Calls_a.csv")
-    # print(call)
+    calls = Calls_List("Calls_a.csv")
+
+    print("the size for calls list is = "+str(calls.cals_size()))
+    print(calls.rows_list)
+
+    calls.set_to_scv()
+  #  one_call = CallForElevator(0,0,0,0)
 
    # call_l = Calls_List("Calls_a.csv")
     #print(call_l.rows_list)
