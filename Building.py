cb67e1cd6aee8ab75573bfaf77d6795786a59d5a
Dima Sanin <34384550+dimastar2310@users.noreply.github.com>
2021-11-15 02:53:29 +0200

    Add files via upload

--- Building.py
+++ Building.py
@@ -1,38 +1,55 @@
 import json
 
-from Elevators import Elevators
+from Elevator import Elevator
 
 
 class Building(object):
 
     def __init__(self, file):
+        self.buildingname = file
 
         self.load_json(file)
 
-    def load_json(self, file):
+
+
+
+    def load_json(self, file) ->None:
+
         elevator_list = []
         with open(file, "r")as f:
-            d = json.load(fp=f)
+            d = json.load(f)
+
             self.minFloor = d["_minFloor"]
             self.maxFloor = d["_maxFloor"]
+            self.elevSize = len(d["_elevators"])
+
             for elev in d["_elevators"]:
-                el = Elevators(id=elev["_id"], speed=elev["_speed"], minFloor=elev["_minFloor"],
+
+
+                el = Elevator(id=elev["_id"], speed=elev["_speed"], minFloor=elev["_minFloor"],
                                maxFloor=elev["_maxFloor"],
                                closeTime=elev["_closeTime"], openTime=elev["_openTime"],
                                startTime=elev["_startTime"],
                                stopTime=elev["_stopTime"])
                 elevator_list.append(el)
+                #caunt_elev = caunt_elev + 1
             self.elevators = elevator_list
+            f.close()
 
-        return self
+
     def getBuildingName(self)->str:
-        pass
+        return self.buildingname
     def minFloor(self)->int:
         pass
     def maxFloor(self)->int:
         pass
     def numberOfElevator(self)->int:
+
         pass
-    def getElevator(self)->Elevators:
-        pass
+    #all elevators
+    def ElevSize(self):
+        return len(self.elevators)
+
+    def getElevator(self,place)->Elevator:
+        return self.elevators[place]
 
--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 
 from Building import Building
 from  Calls_List import Calls_List
-
+from Our_Algo import Our_Algo
 
 def main():
     #b = Building()
     b = Building("B2.json")
     print(b.elevators)
+    print("elevator size is " +str(b.ElevSize()))
 
     #call = CallForElevator()
-   # call.load_csv("Calls_a.csv")
-   # print(call)
+    # call.load_csv("Calls_a.csv")
+    # print(call)
 
-    call_l = Calls_List("Calls_a.csv")
-    print(call_l.rows_list)
+   # call_l = Calls_List("Calls_a.csv")
+    #print(call_l.rows_list)
+    #OA = Our_Algo(b,call_l)
 
 
 
