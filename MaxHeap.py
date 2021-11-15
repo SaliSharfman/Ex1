963412917c352b1419b4c5193dbc49421a2cc46c
Dima Sanin <34384550+dimastar2310@users.noreply.github.com>
2021-11-15 22:52:45 +0200

    Update MaxHeap.py

--- MaxHeap.py
+++ MaxHeap.py
@@ -1,19 +1,11 @@
-#assending and desending order
 
+class MinHeap(object):
+     def __init__(self):
+      self.list = []
 
+      def add(self, num):
+       self.list.append(num)
+       self.sort()
 
-
-
-
-class MaxHeap(object):
-
-    #kodem neathel oto
-    def __init__(self):
-        self.list = []
-        #mispar rishon triviali
-
-     #   add(self,num)
-
-    def add(self, num):
-        self.list.append(num)
-        self.list.sort(reverse=True)
+       def pop(self):
+           self.list.remove(0)
