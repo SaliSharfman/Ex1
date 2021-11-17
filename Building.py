11db27a0843b2ebd9c827562eec40d9cc32c4636
SaliSharfman <86108478+SaliSharfman@users.noreply.github.com>
2021-11-17 20:00:30 +0200

    Add files via upload

--- Building.py
+++ Building.py
@@ -8,33 +8,6 @@ class Building(object):
     def __init__(self, numelevs:int):
        self.elevators=[]
        self.elevSize=numelevs
-
-
-
-    def load_json(self, file) ->None:
-
-        elevator_list = []
-        with open(file, "r")as f:
-            d = json.load(f)
-
-            self.minFloor = d["_minFloor"]
-            self.maxFloor = d["_maxFloor"]
-            self.elevSize = len(d["_elevators"])
-
-            for elev in d["_elevators"]:
-
-
-                el = Elevator(id=elev["_id"], speed=elev["_speed"], minFloor=elev["_minFloor"],
-                               maxFloor=elev["_maxFloor"],
-                               closeTime=elev["_closeTime"], openTime=elev["_openTime"],
-                               startTime=elev["_startTime"],
-                               stopTime=elev["_stopTime"])
-                elevator_list.append(el)
-                #caunt_elev = caunt_elev + 1
-            self.elevators = elevator_list
-            f.close()
-
-
     #all elevators
     def getElevSize(self)->int:
         return self.elevSize
